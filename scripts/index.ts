3822f839a21185aa90d36d6b50f5cbda6db7455f
Mattia Franchetto <mt.franchetto@gmail.com>
2018-02-07 10:53:53 +0100

    Export localized component

--- scripts/index.ts
+++ scripts/index.ts
@@ -5,6 +5,7 @@ export { default as TranslationsRunner } from "./TranslationsRunner";
 export { default as ITranslationsRunner } from "./ITranslationsRunner";
 export { default as ITranslationsConfig } from "./ITranslationsConfig";
 export { default as LocalizedApplication } from "./LocalizedApplication";
+export { default as LocalizedComponent } from "./LocalizedComponent";
 export { default as TranslationsLoader } from "./retrievers/TranslationsLoader";
 export { default as ILanguageRetriever } from "./retrievers/ILanguageRetriever";
 export { default as ITranslationsLoader } from "./retrievers/ITranslationsLoader";
