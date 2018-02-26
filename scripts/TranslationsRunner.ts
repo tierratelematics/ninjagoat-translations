095a31e07d1d5a884c4bd73f03806cdcb211d954
Mattia Franchetto <mt.franchetto@gmail.com>
2018-02-26 15:41:07 +0100

    Add initial optional language

--- scripts/TranslationsRunner.ts
+++ scripts/TranslationsRunner.ts
@@ -4,6 +4,7 @@ import { inject, injectable } from "inversify";
 import ITranslationsRunner from "./ITranslationsRunner";
 import ILanguageRetriever from "./retrievers/ILanguageRetriever";
 import ITranslationsLoader from "./retrievers/ITranslationsLoader";
+import ITranslationsConfig from "./ITranslationsConfig";
 
 export type TranslationsModel = { language: string; translations: Dictionary<string> }
 
@@ -11,11 +12,13 @@ export type TranslationsModel = { language: string; translations: Dictionary<str
 class TranslationsRunner implements ITranslationsRunner {
     private notifications: Observable<TranslationsModel>;
 
-    constructor( @inject("ILanguageRetriever") private languageRetriever: ILanguageRetriever,
-                 @inject("ITranslationsLoader") private translationsLoader: ITranslationsLoader) {
+    constructor(@inject("ILanguageRetriever") private languageRetriever: ILanguageRetriever,
+                @inject("ITranslationsLoader") private translationsLoader: ITranslationsLoader,
+                @inject("ITranslationsConfig") config: ITranslationsConfig) {
         this.notifications = this.languageRetriever.retrieve()
-            .flatMap(lang => this.translationsLoader.load(lang)
-                .then(translations => ({ language: lang, translations: translations })))
+            .startWith(config.language)
+            .filter(language => !!language)
+            .flatMap(lang => this.translationsLoader.load(lang).then(translations => ({ language: lang, translations: translations })))
             .shareReplay(1);
     }
 
