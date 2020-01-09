5f78552e4350127527917fc29e7453dfac736ff3
Enrico Calzavara <enrico.calzavara@gmail.com>
2020-01-09 17:17:59 +0100

    Use translations url builder in translations loader

--- scripts/retrievers/TranslationsLoader.ts
+++ scripts/retrievers/TranslationsLoader.ts
@@ -2,23 +2,22 @@ import ITranslationsLoader from "./ITranslationsLoader";
 import {Dictionary} from "ninjagoat";
 import {injectable, inject} from "inversify";
 import {IHttpClient} from "ninjagoat";
-import ITranslationsConfig from "../ITranslationsConfig";
+import ITranslationsUrlBuilder from "../ITranslationsUrlBuilder";
 
 @injectable()
 class TranslationsLoader implements ITranslationsLoader {
 
-    constructor(@inject("IHttpClient") private httpClient:IHttpClient,
-                @inject("ITranslationsConfig") private config:ITranslationsConfig) {
+    constructor(@inject("IHttpClient") private httpClient: IHttpClient,
+                @inject("ITranslationsUrlBuilder") private urlBuilder: ITranslationsUrlBuilder) {
 
     }
 
     load(language:string):Promise<Dictionary<string>> {
         return <Promise<Dictionary<string>>>this.httpClient
-            .get(`${this.config.endpoint}/${language}.json`)
+            .get(this.urlBuilder.getUrl(language))
             .map(response => response.response)
             .toPromise();
     }
-
 }
 
 export default TranslationsLoader;
